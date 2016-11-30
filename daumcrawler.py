72bd84e2ea2bf5129fc3f1781b4415fe57584a07
BoyoungKim <bnewkk@hanyang.ac.kr>
2016-11-30 15:27:48 +0900

    Update daumcrawler.py

--- daumcrawler.py
+++ daumcrawler.py
@@ -13,21 +13,8 @@ sys.setdefaultencoding('utf-8')
 class MyCrawler(scrapy.Spider):
     name = 'daumcrawler'
 
-    #start_urls = ['http://blog.daum.net/ktg0205/954']
-    #start_urls = ['http://blog.daum.net/ktg0205/954/_blog/BlogTypeView.do?blogid=0eq3B&arti']
-    #start_urls = ['http://blog.daum.net/_blog/BlogTypeView.do?blogid=0eq3B&articleno=954&admin=']
-    #start_urls = ['http://blog.daum.net/_blog/hdn/ArticleContentsView.do?blogid=0eq3B&articleno=954&looping=0&longOpen=']
-
-
-    #start_urls = ['http://blog.daum.net/mi_chan1027/943']
-    #start_urls = ['http://blog.daum.net/mi_chan1027/943/_blog/BlogTypeView.do?blogid=0mS8T&arti']
-    #start_urls = ['http://blog.daum.net/_blog/BlogTypeView.do?blogid=0mS8T&articleno=943&admin=']
+   //after doing frame, iframe
     start_urls = ['http://blog.daum.net/_blog/hdn/ArticleContentsView.do?blogid=0mS8T&articleno=943&looping=0&longOpen=']
-
-    #start_urls = ['http://blog.daum.net/tjrrr0304/15932']
-    #start_urls = ['http://blog.daum.net/_blog/BlogTypeView.do?blogid=0WFoG&articleno=15932&admin=']
-    #start_urls = ['http://blog.daum.net/_blog/hdn/ArticleContentsView.do?blogid=0WFoG&articleno=15932&looping=0&longOpen=']
-
 
     #/_blog/hdn/ArticleContentsView.do?blogid
 
@@ -50,26 +37,7 @@ class MyCrawler(scrapy.Spider):
         for j in text2 :
             print j
 
-        #print text
-        #text = response.css("#contentDiv > p> span::text").extract()
-        #text = response.css("#contentDiv > p > span >  strong > span > span > span > span > span > span > span::text").extract()
-        #text2 = response.css("#contentDiv > p> span > font > strong > span::text").extract()
-        #text3 = response.css("#contentDiv > p> span > font > strong > span > span::text").extract()
-        #text4 = response.css("#contentDiv > p > span > strong > span::text").extract()
-        #text5 = response.css("#contentDiv > p > span > font > strong > span > span").extract()
-        #text6 = response.css("#contentDiv > p > span > strong > span").extract()
-
-
-        #for j in text2 :
-         #   print j
-        #for k in text3 :
-         #   print k
-
-        #for a in text5 :
-         #   print a
-        #for b in text6 :
-         #   print b
-
+       
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
