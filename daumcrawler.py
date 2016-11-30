188e30099dda49f9c9097f2721079e86dd80e734
BoyoungKim <bnewkk@hanyang.ac.kr>
2016-11-30 15:29:09 +0900

    Update daumcrawler.py

--- daumcrawler.py
+++ daumcrawler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import datetime, time
+import httplib, urllib
 from scrapy.crawler import CrawlerProcess
 
 import sys
@@ -13,8 +14,8 @@ sys.setdefaultencoding('utf-8')
 class MyCrawler(scrapy.Spider):
     name = 'daumcrawler'
 
-   //after doing frame, iframe
     start_urls = ['']
+
 
     #/_blog/hdn/ArticleContentsView.do?blogid
 
@@ -24,20 +25,24 @@ class MyCrawler(scrapy.Spider):
         today.replace(hour=int(hour), minute=int(minute), microsecond=0)
         return str(time.mktime(today.timetuple()))
 
+
+
     def parse(self, response):
 
 
         #text = response.css("iframe::attr(src)")
         #text = response.css("frame::attr(src)")
-        text1 = response.css("#contentDiv img::attr(src)").extract()
-        text2 = response.css("#contentDiv > p span::text").extract()
+        img = response.css("#contentDiv img::attr(src)").extract()
+        text = response.css("#contentDiv > p span::text").extract()
 
-        for i in text1 :
+        print "Daum Crawler"
+
+        for i in img :
             print i
-        for j in text2 :
+        for j in text :
             print j
 
-       
+
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
